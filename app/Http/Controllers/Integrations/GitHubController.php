bf19976ac18830f89b2a0d7371fdc2b5555a9bda
chetannn <chetankharel7@gmail.com>
2022-12-08 21:29:21 +0545

    wip

--- app/Http/Controllers/Integrations/GitHubController.php
+++ app/Http/Controllers/Integrations/GitHubController.php
@@ -17,8 +17,17 @@ class GitHubController extends Controller
 
     public function callBack(): RedirectResponse
     {
-        $user = Socialite::driver('integration:github')->user();
+            $user = Socialite::driver('integration:github')->user();
 
+            if(!auth()->user()->sourceProviders()->wherePivot('provider_id', 1)->exists()) {
+                auth()->user()->sourceProviders()->attach(1, ['payload' => json_encode($user), 'label' => $user->getName()]);
+            }
+
+        return to_route('integrations.index');
+    }
+
+    public function disconnect() : RedirectResponse
+    {
         return to_route('integrations.index');
     }
 }
